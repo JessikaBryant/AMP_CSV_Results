fb4ad722caef43ae28fe0f9a6770d02c0587e1f6
bryantj2-osu <bryantj2@oregonstate.edu>
2024-06-10 14:02:17 -0700

    updated .py script to give out a fasta file of only HIGH probability AMP's and a text file that states the percentage/proportion of AMP likely seqs

--- ResultsParser.py
+++ ResultsParser.py
@@ -3,32 +3,119 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from Bio import SeqIO 
 import sys
 
 ###read in files
 amppredictorresults=pd.read_csv("InputFiles/DmollisAMPResults", sep="\t")
-ampfeaturesresults=pd.read_csv("InputFiles/20240605_scored_features.csv", sep=",")
+#ampfeaturesresults=pd.read_csv("InputFiles/20240605_scored_features.csv", sep=",")
 #print(len(ampfeaturesresults))
-#print(len(amppredictorresults))
+#print(amppredictorresults.keys())
+#sys.exit()
 
 ###add probability of AMP from amppredictedresults as a column to ampfeatures
 #make empty column for new column
-ampfeaturesresults.loc[:,"probability_AMP"]=""
+#ampfeaturesresults.loc[:,"probability_AMP"]=""
 #print(ampfeaturesresults.keys())
 #add probability amp to ampfeatures
-ampfeaturesresults["probability_AMP"]=amppredictorresults["probability_AMP"].to_numpy()
+#ampfeaturesresults["probability_AMP"]=amppredictorresults["probability_AMP"].to_numpy()
 #print(ampfeaturesresults[0:5])
 
-seqswithhighprobs=dict()
+seqswithhighprobs=list()
+counter=0
+
 
 #find amp_probability >0.8
-for probability in ampfeaturesresults["probability_AMP"]:
-    if probability>=0.75:
-        seqswithhighprobs[str(ampfeaturesresults["sequence_name"])]=""
-        seqswithhighprobs[str(ampfeaturesresults["sequence_name"])]=probability
-    print(seqswithhighprobs())
-    break
+#loop through every row in dataframe
+for rownumber in amppredictorresults.index:
+    tempProb=amppredictorresults.loc[rownumber]["probability_AMP"]
+    tempSeqId=amppredictorresults.loc[rownumber]["seq_id"]
+    if tempProb>=0.85:
+        seqswithhighprobs.append(tempSeqId)
+    
+    counter+=1
+    if counter>100000:
+        break
+
+###Read in the clean fasta file and find the seqs with high probability
+# Read in the DNA sequence associated with the annotations
+
+#Get the full path to the DNA sequence
+seq_file_path = "/scratch/bryantj2/bb485/week09/CleanGenomes/DmollisOUT/GCA_032361265.1_ASM3236126v1_genomic_squeak.fa"
+
+seq_file_handle = open(seq_file_path, "r")
+
+#Create an empty dictionary
+seq_dict = {}
+
+#Loop through the line in the file
+for line in seq_file_handle:
+    if line.startswith(">"):
+        id_temp = line.strip() #Removes "\n"
+        id_clean = id_temp.replace(">", "") #Removes ">" by replacing with nothing.
+        
+        #Add the item to the dictionary
+        seq_dict[id_clean]="" # id_clean is the key, the value is an empty string (for now)
+    else:
+        seq_line = line.strip() #Removes "\n"
+        
+        #append this line to the dictionary value, using the key (which is still "id_clean" from the previous line)
+        seq_dict[id_clean] += seq_line
+
+
+keepers_dict=dict()
+
+for k, v in seq_dict.items():
+    if k in seqswithhighprobs:
+        keepers_dict[k]=v
+
+
+#proportion of sequences kept (probabiilty >=0.8) from total sequences input
+proportionkeepseqs=len(keepers_dict.keys())/len(seq_dict)
+#print(proportionkeepseqs)
+
+proportionhandle=open("PredictedAMP_Proportion.txt", "a")
+proportionhandle.write("The proportion of AMPs predicted by amPEPpy is " + str(proportionkeepseqs) +" for Dmollis")
+proportionhandle.close()
+
+
+handle=open("DmollisPredictedAMPs.fasta", "a")
+
+for k, v in keepers_dict.items():
+    handle.write(">" + k +"\n") 
+    handle.write(v + "\n")
+
+handle.close()
+
 sys.exit()
+
+
+"""
+with open("Test1.fasta", "w") as handle:
+    SeqIO.write(kee.values(), handle, 'fasta')
+
+sys.exit()"""
+
+#loop through the amp_probability for seqs greater than 0.7 and add seq names either to a list or a dict w/ probability
+
+
+
+"""for seqname in amppredictorresults["seq_id"]:
+    tempseqdict=dict()
+    tempseqdict[str(seqname)]=""    
+    for probability in amppredictorresults["probability_AMP"]:
+        tempseqdict[seqname]=probability
+        #print(tempseqdict)
+        if probability>=0.7:
+            seqswithhighprobs=tempseqdict[seqname]
+            print(seqswithhighprobs)
+            break
+        sys.exit()"""
+            
+        
+
+
+
 
 ###Make scatter plot 
 #x=x-axis = probability amp
